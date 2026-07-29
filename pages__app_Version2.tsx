acd72e72d7d9fa373c9ebb0495e80f61484368e5
Manoj <golllffra@gmail.com>
2026-07-29 12:48:59 +0545

    chore: import animated background CSS and mount AnimeBackground in app wrapper

--- pages__app_Version2.tsx
+++ pages__app_Version2.tsx
@@ -1,6 +1,8 @@
 // If your project uses pages/ router, modify pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+// load the animated background CSS we added
+import "../styles/animated-background.css";
 import AnimeBackground from "../components/AnimeBackground";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
